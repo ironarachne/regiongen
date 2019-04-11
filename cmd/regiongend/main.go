6a54257ad2c8b5e84327515c398ad9f0f45cd0bd
Ben Overmyer <manatrance@gmail.com>
2019-04-11 12:24:45 -0500

    Convert to chi. Update the towngen library

--- cmd/regiongend/main.go
+++ cmd/regiongend/main.go
@@ -1,67 +1,80 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
-	"strconv"
+	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/ironarachne/random"
 	"github.com/ironarachne/regiongen"
-	"github.com/kataras/iris"
+
 	"github.com/patrickmn/go-cache"
 )
 
 func main() {
-	app := iris.New()
-	c := cache.New(5*time.Minute, 10*time.Minute)
+	r := chi.NewRouter()
+	centralCache := cache.New(5*time.Minute, 10*time.Minute)
 
-	app.Get("/", func(ctx iris.Context) {
-		ctx.Writef("regiongend")
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(middleware.SetHeader("Content-Type", "application/json"))
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().UnixNano())
+
+		region := regiongen.GenerateRegion("random")
+
+		json.NewEncoder(w).Encode(region)
 	})
 
-	app.Get("/{id:int64}", func(ctx iris.Context) {
-		id, err := ctx.Params().GetInt64("id")
-		if err != nil {
-			ctx.Writef("error while trying to parse id parameter")
-			ctx.StatusCode(iris.StatusBadRequest)
-			return
-		}
+	r.Route("/{id}", func(r chi.Router) {
+		r.Get("/heraldry", func(w http.ResponseWriter, r *http.Request) {
+			id := chi.URLParam(r, "id")
 
-		region, found := c.Get("region_" + strconv.FormatInt(id, 10))
+			heraldry, found := centralCache.Get("heraldry_" + id)
 
-		if found {
-			ctx.JSON(region)
-		} else {
-			rand.Seed(id)
-			region := regiongen.GenerateRegion("random")
-			c.Set("region_"+strconv.FormatInt(id, 10), region, cache.DefaultExpiration)
+			if found {
+				w.Header().Set("Content-Type", "image/svg+xml")
+				w.Write([]byte(heraldry.(string)))
+			} else {
+				random.SeedFromString(id)
+				region := regiongen.GenerateRegion("random")
+				centralCache.Set("region_"+id, region, cache.DefaultExpiration)
+				centralCache.Set("heraldry_"+id, region.RulerHeraldry, cache.DefaultExpiration)
 
-			ctx.JSON(region)
-		}
+				w.Header().Set("Content-Type", "image/svg+xml")
+				w.Write([]byte(region.RulerHeraldry))
+			}
+		})
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			id := chi.URLParam(r, "id")
 
+			region, found := centralCache.Get("region_" + id)
+
+			if found {
+				json.NewEncoder(w).Encode(region)
+			} else {
+				random.SeedFromString(id)
+
+				region := regiongen.GenerateRegion("random")
+				centralCache.Set("region_"+id, region, cache.DefaultExpiration)
+				centralCache.Set("heraldry_"+id, region.RulerHeraldry, cache.DefaultExpiration)
+
+				json.NewEncoder(w).Encode(region)
+			}
+		})
 	})
 
-	app.Get("/{id:int64}/heraldry.svg", func(ctx iris.Context) {
-		id, err := ctx.Params().GetInt64("id")
-		if err != nil {
-			ctx.Writef("error while trying to parse id parameter")
-			ctx.StatusCode(iris.StatusBadRequest)
-			return
-		}
-
-		region, found := c.Get("region_" + strconv.FormatInt(id, 10))
-
-		if found {
-			regionData := region.(regiongen.Region)
-			ctx.ContentType("image/svg+xml")
-			ctx.Writef(regionData.RulerHeraldry)
-		} else {
-			rand.Seed(id)
-			region := regiongen.GenerateRegion("random")
-			c.Set("region_"+strconv.FormatInt(id, 10), region, cache.DefaultExpiration)
-			ctx.ContentType("image/svg+xml")
-			ctx.Writef(region.RulerHeraldry)
-		}
-	})
-
-	app.Run(iris.Addr(":7970"))
+	fmt.Println("Region Generator API is online.")
+	log.Fatal(http.ListenAndServe(":7970", r))
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,15 @@
 module github.com/ironarachne/regiongen
 
 require (
-	github.com/ironarachne/chargen v0.0.0-20190309165134-658ccecdd5a6
-	github.com/ironarachne/climategen v0.0.0-20190309163423-cddd04bd0fd1
-	github.com/ironarachne/heraldry v0.0.0-20190309155546-459cda15e070
-	github.com/ironarachne/orggen v0.0.0-20190307143258-85d9edc87c44
-	github.com/ironarachne/placenamegen v0.0.0-20190311181634-645604419b88
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/ironarachne/chargen v0.0.0-20190325171707-70291dcd719e
+	github.com/ironarachne/climategen v0.0.0-20190411160729-21e7b32e3cb2
+	github.com/ironarachne/heraldry v0.0.0-20190325172346-07bf7eeae990
+	github.com/ironarachne/orggen v0.0.0-20190325172603-1073880e3a9d
+	github.com/ironarachne/placenamegen v0.0.0-20190325170957-e24f4e03e8ad
 	github.com/ironarachne/random v0.0.0-20190325170425-9caaa0b3405a
-	github.com/ironarachne/towngen v0.0.0-20190309165237-f6e2bead37d6
+	github.com/ironarachne/towngen v0.0.0-20190411162951-4ae1b95ace19
 	github.com/ironarachne/utility v0.0.0-20181021125830-32a6c250e33b
+	github.com/kataras/iris v11.1.1+incompatible
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
